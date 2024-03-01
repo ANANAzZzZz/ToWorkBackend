c1d4f1662d740b962c9647c2e1c3c2ee031de465
ANANAzZzZz <vladscooltrol@gmail.com>
2024-03-01 18:43:56 +0300

    add user with progress to db

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'abcde'
     DB_SERVER = os.environ.get('DB_SERVER') or 'localhost'
